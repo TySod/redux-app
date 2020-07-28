1e608179b248d2bcca386733c326046ff7126594
blacksuperdev <oyeniyi4real@gmail.com>
2020-07-28 12:47:46 +0100

    post fetched

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Posts from './components/Posts'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="App">   
+        <Posts/>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,7 +13,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      
     </div>
   );
 }
