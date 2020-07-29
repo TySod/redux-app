9b04fcc1d338019370aff890f517133fe0c0c7ad
blacksuperdev <oyeniyi4real@gmail.com>
2020-07-29 13:34:22 +0100

    created form in postform and installed redux...

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import './App.css';
 import Posts from './components/Posts'
+import PostForm from './components/PostForm'
+import {Provider} from 'react-redux'
 function App() {
   return (
-    <div className="App">   
+    <Provider>
+    <div className="App">
+      <PostForm/>
+      <hr/>
         <Posts/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      
     </div>
+          </Provider>
   );
 }
 
--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react'
 
 export default class PostForm extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            title: '',
+            body: ''
+        }
+    }
+            changeHandler = (e) => {
+              const {value, name} = e.target
+              this.setState({[name] :value}) 
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault()
+        const post = {
+            title: this.state.title,
+            body: this.state.body
+        }
+        fetch("https://jsonplaceholder.typicode.com/posts", {
+            method:'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(post)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
     render() {
         return (
             <div>
+                <h1>Add Post</h1>
+                <form onSubmit={this.onSubmit}>
+                    <div>
+                        <label>Title:</label><br/>
+                        <input type="text" name="title" value={this.state.title} onChange={this.changeHandler}/>
+                    </div>
+                    <div>
+                        <label>Body:</label><br/>
+                        <textarea name="body" value={this.state.body} onChange={this.changeHandler}/>
+                    </div>
+                    <button>Submit</button>
+                </form>
                 
             </div>
         )
